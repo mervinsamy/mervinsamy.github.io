f103edc8a7b21a4ffe3c42cd2ad17177a71e5e88
unknown <mervin.samy@gmail.com>
2017-08-09 02:24:48 +0800

    added scss styles & gulp tasks

---
+++ Gulpfile.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+	return gulp.src('sass/**/*.scss')
+		.pipe(sass.sync().on('error', sass.logError))
+		.pipe(gulp.dest('./css/'));
+});
+
+/*Watch Task*/
+gulp.task('default', function(){
+	gulp.watch('./sass/**/*.scss',['sass']);
+});
