03fb3afb9b290b0f3ba40869549d7555b2fff4b1
unknown <mervin.samy@gmail.com>
2017-08-11 02:41:48 +0800

    added task runner for autoprefixing and sourcemaps

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,14 +2,42 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
+var input = './sass/**/*.scss';
+var output = './css/';
+
+var sassOptions = {
+ errLogToConsole: true,
+ outputStyle: 'expanded'
+};
+
+/*Sass Tasks*/
 gulp.task('sass', function(){
-	return gulp.src('sass/**/*.scss')
-		.pipe(sass.sync().on('error', sass.logError))
-		.pipe(gulp.dest('./css/'));
+	return gulp
+	    .src(input)
+	    .pipe(sourcemaps.init())
+		.pipe(sass(sassOptions).on('error', sass.logError))
+		.pipe(sourcemaps.write())
+		.pipe(autoprefixer())
+		.pipe(gulp.dest(output));
 });
 
 /*Watch Task*/
-gulp.task('default', function(){
-	gulp.watch('./sass/**/*.scss',['sass']);
+gulp.task('watch', function(){
+	return gulp
+	     .watch(input, ['sass'])
+	     .on('change', function(event){
+	     console.log('File' + event.path + 'was ' + event.type + ' running tasks...');
+	     });
+
 });
+
+
+gulp.task('default', ['sass', 'watch']);
+
+
+//gulp.task('default', function(){
+//	gulp.watch('./sass/**/*.scss',['sass']);
+//});
